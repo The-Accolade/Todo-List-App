372d0c53de26aa03895ea67fa3e05f99dea80256
Jerrycaffe <jerrycaffe@gmail.com>
2020-06-05 00:56:32 +0100

    all bug complains fixed and redux state was applied successfuly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./redux/reducers";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+let store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => store.getState());
 
 ReactDOM.render(
   <div>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
